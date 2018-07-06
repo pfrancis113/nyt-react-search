b00b9ec7b8e38caeec210fbb148a5ce03327e1e1
Patrick Francis <pdfrancis3@gmail.com>
2018-07-05 22:55:10 -0400

    minor changes

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
-// const logger = require("morgan"); //logs HTTP methods
-
 
 // Configure body parser for AJAX requests
 app.use(express.urlencoded({extended:true}));
@@ -12,25 +10,14 @@ app.use(express.json());
 // Serve up static assets
 app.use(express.static("client/build"));
 
-// Use morgan logger for logging requests
-// app.use(logger("dev"));
-
 // Add routes
 const routes = require("./routes")
 app.use(routes);
 
 //Determine if using local DB or heroku mLabs db
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact"
-
 var db = process.env.MONGODB_URI || "mongodb://localhost/nytreact"
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
-
-
-// //Connect to database
-// mongoose.connect(
-//   MONGODB_URI
-// );
 
 // Connect mongoose to our database
 mongoose.connect(db, function(error){
